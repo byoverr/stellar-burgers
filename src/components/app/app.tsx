3d142f10e1f7311b33717ed7e824e00387678790
Sergey Shchetinkin <samara.serejka@gmail.com>
2025-05-30 11:37:33 +0300

    add: routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,138 @@
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import { ConstructorPage } from '@pages';
-import '../../index.css';
+import { Feed } from '@pages';
+import { Login } from '@pages';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Profile } from '@pages';
+import { ProfileOrders } from '@pages';
+import { NotFound404 } from '@pages';
+import { Modal } from '@components';
+import { OrderInfo } from '@components';
+import { IngredientDetails } from '@components';
+import { ProtectedRoute } from '@components';
+import { AppHeader } from '@components';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+function App() {
+  const location = useLocation();
+  const background = location.state?.background;
+  const navigate = useNavigate();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  const handleModalClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+
+      <main className={styles.main}>
+        <Routes location={background || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/feed/:number' element={<OrderInfo />} />
+          <Route path='/ingredients/:id' element={<IngredientDetails />} />
+
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/forgot-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/profile/orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <OrderInfo />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </main>
+
+      {/* Модальные окна */}
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal onClose={handleModalClose} title='Детали заказа'>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal onClose={handleModalClose} title='Детали ингредиента'>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal onClose={handleModalClose} title='Детали заказа'>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+}
 
 export default App;
